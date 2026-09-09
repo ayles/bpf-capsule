869e760c59a7f2fc413abffbb27e6055a27cd593
ayles <selya.main@gmail.com>
2026-09-09 10:47:46 +0000

    Format the parameter annotation declaration

--- src/pass/common.h
+++ src/pass/common.h
@@ -18,8 +18,8 @@ llvm::DIType* BtfGetByteArrayPointer(llvm::DIBuilder& builder, uint64_t sizeByte
 
 // paramAnnotations, when given, holds one entry per parameter: null or the
 // DINodeArray of that parameter's annotations (BTF decl tags).
-void BtfFunctionAddDebugInfo(llvm::DIBuilder& debugBuilder, llvm::Function& func, llvm::ArrayRef<llvm::Metadata*> paramTypes,
-    llvm::ArrayRef<llvm::MDNode*> paramAnnotations = {});
+void BtfFunctionAddDebugInfo(
+    llvm::DIBuilder& debugBuilder, llvm::Function& func, llvm::ArrayRef<llvm::Metadata*> paramTypes, llvm::ArrayRef<llvm::MDNode*> paramAnnotations = {});
 
 // The metadata kind, operand-bundle, and string-attribute names the passes
 // use to talk to each other. Linkage-visible symbol names live in
